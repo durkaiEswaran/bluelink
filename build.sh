992332fc42c312b536582f3699b540d6634c5cc5
Durkai Eswaran R <eswarandurkai@gmail.com>
2026-05-16 12:53:20 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ set -o errexit
 
 pip install --upgrade pip
 pip install -r requirements.txt
+python manage.py migrate contenttypes
+python manage.py migrate users
 python manage.py migrate
 python manage.py collectstatic --no-input
 python manage.py create_admin \
